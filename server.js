9addc96c20348a90f8276fd62810ac141f0c98dd
Diego Kaku <diegokaku@gmail.com>
2022-02-28 22:13:21 -0600

    end lesson 1, Heroku setup

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const { query } = require('express');
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 
 const PORT = process.env.PORT || 3001;
 
@@ -44,12 +46,26 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
 app.listen(PORT, () => {

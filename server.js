fc83a87229e1c3d5a0dc3b648b1a031a968298bc
Diego Kaku <diegokaku@gmail.com>
2022-03-02 21:51:23 -0600

    front-end fix

--- server.js
+++ server.js
@@ -112,21 +112,4 @@ app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
 
-fetch('/api/animals', {
-  method: 'POST',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(animalObject),
-})
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    alert('Error: ' + response.statusText);
-  })
-  .then((postResponse) => {
-    console.log(postResponse);
-    alert('Thank you for adding an animal!');
-  });
+
